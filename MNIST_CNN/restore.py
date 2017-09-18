b716d2b4330f9cf819723360d3323cd9a36d31bb
LCTY <mike19951208@gmail.com>
2017-09-18 17:30:25 +0800

    restore.py現在讀取後可以正確執行預測動作

--- MNIST_CNN/restore.py
+++ MNIST_CNN/restore.py
@@ -1,12 +1,22 @@
 import os
 import tensorflow as tf
+from tensorflow.examples.tutorials.mnist import input_data
 
-save_path = os.getcwd() + "/tmp"
+mnist = input_data.read_data_sets("MNIST_data/", one_hot=True)
+
 tf.reset_default_graph()
 with tf.Session() as sess:
+    save_path = os.getcwd() + "/tmp"
     saver = tf.train.import_meta_graph(save_path + '/model.ckpt.meta')
     saver.restore(sess, tf.train.latest_checkpoint(save_path))
-    sess.run(tf.global_variables_initializer())
-    all_vars = tf.trainable_variables()
-    for v in all_vars:
-        print("%s with value %s" % (v.name, sess.run(v)))
+
+    x = tf.get_default_graph().get_tensor_by_name('x:0')
+    y_ = tf.get_default_graph().get_tensor_by_name('y_:0')
+    keep_prob = tf.get_default_graph().get_tensor_by_name('keep_prob:0')
+    y_conv = tf.get_default_graph().get_tensor_by_name('y_conv:0')
+
+    batch = mnist.train.next_batch(200)
+    correct_prediction = tf.equal(tf.argmax(y_conv, 1), tf.argmax(y_, 1))
+    accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+    train_accuacy = accuracy.eval(feed_dict={x: batch[0], y_: batch[1], keep_prob: 1.0})
+    print(train_accuacy)
